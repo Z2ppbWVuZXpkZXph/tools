a79fee49b5adb22c8099a508130b3766614aed6d
Z2ppbWVuZXpkZXph <62645524+Z2ppbWVuZXpkZXph@users.noreply.github.com>
2022-05-28 13:09:22 -0500

    Create Alaska-Calculadora-simple-suma.py
    Programa creado para calcular sumas de manera fácil, insertando 2 dígitos que el cliente inserte.

---
+++ Ciencia-de-datos/Alaska-Calculadora-simple-suma.py
@@ -0,0 +1,7 @@
+#Creado por Alaska
+
+print ("Bienvenido a esta calculadora de sumas")
+w = int(input("Ingrese su primer numero a sumar:"))
+z = int(input("Ingrese su segundo numero a sumar:"))
+
+print ("El resultado de su suma es:", w+z)
